07021a76b5c54fe29d1ab56940a5fdf8c3622e7f
Hemant Raj <coder.hemantraj05@gmail.com>
2025-01-02 02:48:56 +0530

    Create Post Section added

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -1,32 +1,30 @@
+
 "use server";
 
 import prisma from "@/lib/prisma";
 import { auth, currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 
 export async function syncUser() {
   try {
     const { userId } = await auth();
     const user = await currentUser();
 
-    if (!user || !userId) return;
+    if (!userId || !user) return;
 
-    //check if user exists
     const existingUser = await prisma.user.findUnique({
       where: {
         clerkId: userId,
       },
     });
 
-    if (existingUser) {
-      return existingUser;
-    }
+    if (existingUser) return existingUser;
 
     const dbUser = await prisma.user.create({
       data: {
         clerkId: userId,
         name: `${user.firstName || ""} ${user.lastName || ""}`,
-        username:
-          user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+        username: user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
         email: user.emailAddresses[0].emailAddress,
         image: user.imageUrl,
       },
@@ -34,7 +32,7 @@ export async function syncUser() {
 
     return dbUser;
   } catch (error) {
-    console.log("Error in SyncUser", error);
+    console.log("Error in syncUser", error);
   }
 }
 
@@ -53,4 +51,15 @@ export async function getUserByClerkId(clerkId: string) {
       },
     },
   });
+}
+
+export async function getDbUserId() {
+  const { userId: clerkId } = await auth();
+  if (!clerkId) return null;
+
+  const user = await getUserByClerkId(clerkId);
+
+  if (!user) throw new Error("User not found");
+
+  return user.id;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import CreatePost from "@/components/CreatePost";
+import { currentUser } from "@clerk/nextjs/server";
+
 export default async function Home() {
+
+  const user = await currentUser();
   return (
-    <div className="m-4">
-      <h1>
-        Home Page content 
-      </h1>
-    </div>
+    <>
+      <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+        {user? <CreatePost />:null}
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        Who To Follow
+      </div>
+    </>
   );
 }
