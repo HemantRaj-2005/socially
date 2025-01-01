8cd31758c172460beaedc6199be0b2bddfdfa536
Hemant Raj <coder.hemantraj05@gmail.com>
2025-01-02 00:45:05 +0530

    Authentication setup and Added Shadcn to the website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+import { ModeToggle } from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello home page</h1>
+    <div className="m-4">
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+
+     <ModeToggle /> 
+
+      <Button  variant={"secondary"}>Click Me</Button>
     </div>
   );
 }
